4c4b047821d05c1816eda8f19d40dd9886eedadf
Geok Fong <79799984+geokfong@users.noreply.github.com>
2024-12-04 18:05:44 +0800

    Update 3. eTAM_analysis.sh

--- 3. eTAM_analysis.sh
+++ 3. eTAM_analysis.sh
@@ -1,5 +1,6 @@
 #Step 3: Data Preparation and Conversion to .bed
-parent_folder='/home/onggf/data/2_eTAM/result/'
+parent_folder='/path/to/result/'
+mkdir bed
 
 for file in "$parent_folder"*/; do
     echo ${file} 
@@ -11,22 +12,22 @@ for file in "$parent_folder"*/; do
     echo "$hg38_pile"
     echo "$sample"
 
-    cd /home/onggf/data/2_eTAM/result/bed 
+    cd /path/to/result/bed 
     mkdir -p "$sample"
     cd "$sample"
 
     ##rRNA conversion
     tail -n +2 "$rna_pile" | awk -F"\t" '{OFS="\t"; if(($4 == "A")){if($5 > 9){print $1,$2-1,$2,$1"="$2"="$3"="$5"="$6"="$9,$9/$5,$3}}}' - | awk -F"\t" '{OFS="\t"; print $1,($2-2),($3+2),$4,$5,$6}' | awk -F"\t" '{OFS="\t"; if($2>0){print $0}}' > "$sample.rna.tmp.bed"
-    bedtools getfasta -fi /home/onggf/data/2_eTAM/rRNA/rRNA.fasta -bed "$sample.rna.tmp.bed" -nameOnly -tab -s | awk -F"\t" '{OFS="\t"; if(length($2)==5){a=toupper($2);gsub(/T/,"U",a); if(a~/[GAU][AG]AC[ACU]/) {m="DRACH"} else {m="nonDRACH"}; print $0"\t"m}}' | perl -pe 's/\([+-]\)//g'  | awk -F"\t" '{OFS="\t"; split($1, info, "="); print info[1],info[2]-1,info[2],$1"="$2"="$3,info[6]/info[4],info[3]}' > "$sample.rRNA.Conversions.bed"
+    bedtools getfasta -fi /path/to/rRNA/rRNA.fasta -bed "$sample.rna.tmp.bed" -nameOnly -tab -s | awk -F"\t" '{OFS="\t"; if(length($2)==5){a=toupper($2);gsub(/T/,"U",a); if(a~/[GAU][AG]AC[ACU]/) {m="DRACH"} else {m="nonDRACH"}; print $0"\t"m}}' | perl -pe 's/\([+-]\)//g'  | awk -F"\t" '{OFS="\t"; split($1, info, "="); print info[1],info[2]-1,info[2],$1"="$2"="$3,info[6]/info[4],info[3]}' > "$sample.rRNA.Conversions.bed"
 
     ##hg38 conversion
     tail -n +2 "$hg38_pile" | awk -F"\t" '{OFS="\t"; if(($4 == "A")){if($5 > 9){print $1,$2-1,$2,$1"="$2"="$3"="$5"="$6"="$9,$9/$5,$3}}}' - | awk -F"\t" '{OFS="\t"; print $1,($2-2),($3+2),$4,$5,$6}' | awk -F"\t" '{OFS="\t"; if($2>0){print $0}}' > "$sample.hg38.tmp.bed"
-    bedtools getfasta -fi /home/onggf/data/2_eTAM/hg38/Homo_sapiens/NCBI/GRCh38/Sequence/WholeGenomeFasta/genome.fa -bed "$sample.hg38.tmp.bed" -nameOnly -tab -s | awk -F"\t" '{OFS="\t"; if(length($2)==5){a=toupper($2);gsub(/T/,"U",a); if(a~/[GAU][AG]AC[ACU]/) {m="DRACH"} else {m="nonDRACH"}; print $0"\t"m}}' | perl -pe 's/\([+-]\)//g'  | awk -F"\t" '{OFS="\t"; split($1, info, "="); print info[1],info[2]-1,info[2],$1"="$2"="$3,info[6]/info[4],info[3]}' > "$sample.hg38.Conversions.bed"
+    bedtools getfasta -fi /path/to/hg38/Homo_sapiens/NCBI/GRCh38/Sequence/WholeGenomeFasta/genome.fa -bed "$sample.hg38.tmp.bed" -nameOnly -tab -s | awk -F"\t" '{OFS="\t"; if(length($2)==5){a=toupper($2);gsub(/T/,"U",a); if(a~/[GAU][AG]AC[ACU]/) {m="DRACH"} else {m="nonDRACH"}; print $0"\t"m}}' | perl -pe 's/\([+-]\)//g'  | awk -F"\t" '{OFS="\t"; split($1, info, "="); print info[1],info[2]-1,info[2],$1"="$2"="$3,info[6]/info[4],info[3]}' > "$sample.hg38.Conversions.bed"
 
 done
 
 ## Step 4: Generate eTAM Input Files
-parent_folder='/home/onggf/data/2_eTAM/result/bed/'
+parent_folder='path/to/result/bed/'
 
 for gen in WT KO; do 
     for treatment in UT BT2 BT4; do 
@@ -38,14 +39,14 @@ for gen in WT KO; do
 done
 
 #Step 5: Run R script to generate output
-cd /home/onggf/data/2_eTAM/result/bed/result/
+cd /path/to/result/bed/result/
 mkdir final_output
 
 for gen in WT KO; do 
     for treatment in UT BT2 BT4; do 
         for rep in 1 2; do 
-            input_file="/home/onggf/data/2_eTAM/result/bed/result/${gen}_${treatment}_${rep}.eTAMInput.tsv"
-            output_file="/home/onggf/data/2_eTAM/result/bed/result/final_output/${gen}_${treatment}_${rep}.ivtcount.table.out.txt"
+            input_file="/path/to/result/bed/result/${gen}_${treatment}_${rep}.eTAMInput.tsv"
+            output_file="/path/to/result/bed/result/final_output/${gen}_${treatment}_${rep}.ivtcount.table.out.txt"
             echo "Running ${gen}_${treatment}_${rep}";
             Rscript run_model_ivt.R "$input_file" "$output_file" 50000 T 123 N 10 0.05 0.1
         done; 
